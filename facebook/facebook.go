56349ac798b6f558f25abcbb359163e07401cc77
Silkage <silkage@silkage.net>
2022-05-16 18:18:06 +0800

    add facebook oauth token verify

---
+++ facebook/facebook.go
@@ -0,0 +1,103 @@
+// nolint: gosec
+package facebook
+
+import (
+	"context"
+	"fmt"
+	"github.com/pinealctx/restgo"
+	"time"
+)
+
+const (
+	OAuthTokenURL = "https://graph.facebook.com/oauth/access_token"
+	DebugTokenURL = "https://graph.facebook.com/debug_token"
+)
+
+type Client struct {
+	appid     string
+	appSecret string
+
+	cli *restgo.Client
+}
+
+func New(appid, appSecret string) *Client {
+	return &Client{
+		appid:     appid,
+		appSecret: appSecret,
+		cli:       restgo.New(),
+	}
+}
+
+func (c *Client) VerifyToken(ctx context.Context, token string) (*DebugTokenResponse, error) {
+	var accessToken, err = c.accessToken(ctx)
+	if err != nil {
+		return nil, err
+	}
+	var debugToken *DebugTokenResponse
+	debugToken, err = c.debugToken(ctx, token, accessToken.AccessToken)
+	if err != nil {
+		return nil, err
+	}
+	return debugToken, nil
+}
+
+type DebugTokenResponseData struct {
+	AppID       int64                  `json:"app_id"`
+	Type        string                 `json:"type"`
+	Application string                 `json:"application"`
+	ExpiresAt   int64                  `json:"expires_at"`
+	IsValid     bool                   `json:"is_valid"`
+	IssuedAt    int64                  `json:"issued_at"`
+	Metadata    map[string]interface{} `json:"metadata"`
+	Scopes      []string               `json:"scopes"`
+	UserID      string                 `json:"user_id"`
+}
+
+type DebugTokenResponse struct {
+	DebugTokenResponseData `json:"data"`
+}
+
+func (c *Client) debugToken(ctx context.Context, inputToken, accessToken string) (*DebugTokenResponse, error) {
+	var response, err = c.cli.Get(ctx, DebugTokenURL,
+		restgo.NewURLQueryParam("input_token", inputToken),
+		restgo.NewURLQueryParam("access_token", accessToken),
+	)
+	if err != nil {
+		return nil, err
+	}
+	var rsp DebugTokenResponse
+	err = response.JSONUnmarshal(&rsp)
+	if err != nil {
+		return nil, err
+	}
+	var now = time.Now().Unix()
+	if !rsp.IsValid {
+		return nil, fmt.Errorf("invalid token")
+	}
+	if rsp.ExpiresAt < time.Now().Unix() {
+		return nil, fmt.Errorf("token already expired, expires_at(%d) < now(%d)", rsp.ExpiresAt, now)
+	}
+	return &rsp, nil
+}
+
+type AccessTokenResponse struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+}
+
+func (c *Client) accessToken(ctx context.Context) (*AccessTokenResponse, error) {
+	var response, err = c.cli.Get(ctx, OAuthTokenURL,
+		restgo.NewURLQueryParam("client_id", c.appid),
+		restgo.NewURLQueryParam("client_secret", c.appSecret),
+		restgo.NewURLQueryParam("grant_type", "client_credentials"),
+	)
+	if err != nil {
+		return nil, err
+	}
+	var rsp AccessTokenResponse
+	err = response.JSONUnmarshal(&rsp)
+	if err != nil {
+		return nil, err
+	}
+	return &rsp, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/pinealctx/oauth2
 
 go 1.17
+
+require (
+	github.com/fatih/structtag v1.2.0 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/pinealctx/neptune v0.8.4-0.20220513082456-ea94fa5e23b9 // indirect
+	github.com/pinealctx/restgo v0.1.4-0.20220514031817-6e729de7652d // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.19.0 // indirect
+)
